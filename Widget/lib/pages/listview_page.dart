2ce2f5e71e658d47e004d89fa5557e49d9e5de94
Ahmet Kaya <academyakm0@gmail.com>
2025-02-12 15:35:02 +0300

    Day 1 - Day 2 Dart and Widget

---
+++ Widget/lib/pages/listview_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ListViewPage extends StatelessWidget{
+  final List<String> names = ["Ahmet","Mustafa","Ali","Ayşe","Bengisu"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ListView Widget"),
+      ),
+      body: ListView.builder(
+          itemCount: names.length,
+        itemBuilder: (context,index){
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text(names[index][0]),
+              ),
+              title: Text(names[index]),
+              subtitle: Text("Kullanıcı ${index + 1}"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: (){
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("${names[index]} seçildi")),
+                );
+              },
+
+            );
+        },
+      ),
+    );
+  }
+}
