86a105aa9831b2ec15ae56be8b39bbecd0a642d6
Ahmet Kaya <academyakm0@gmail.com>
2025-02-14 14:32:10 +0300

    Home Page Tabs

---
+++ yolcu360/lib/pages/campaing_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CampaingPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Kampanyalar"),
+      ),
+      body: Text("Kampanya Sayfası"),
+    );
+  }
+}
